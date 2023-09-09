b1e4892017f730a1dabd64e510159e3a30500471
Nadeem740 <nadeemblayparambil@gmail.com>
2023-09-09 14:46:57 +0530

    dock-booking-final

--- src/Pages/Shipments/ListAllShipmentsPage.js
+++ src/Pages/Shipments/ListAllShipmentsPage.js
@@ -5,38 +5,32 @@ import axios from "axios";
 import { baseUrl } from "../../utils/baseurl";
 const ListAllShipmentsPage = ({iseditable}) => {
     const {setLoading}=useContext(UserContext);
-    const [companyData,setCompanyData]=useState([]);
+    const [todayShipments,setTodayShipments]=useState([])
+
     useEffect(()=>{
         setLoading(true);
         const token = localStorage.getItem("EZTOken");
-        axios
-      .get(`${baseUrl}/get-companies`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(function (response) {
+        axios.get(`${baseUrl}/dock/current/booking/users`, {headers: {
+            Authorization: `Bearer ${token}`,
+          }},)
+        .then((res) => {
+        if(res.data.status==="ok")
+          {  
+            console.log(res.data.data)
+            setTodayShipments(res.data.data);
+          }
+        })
+        .catch((err) => {
+            console.log(err,"error");
+        
+        });
         setLoading(false);
-
-        console.log("success", response, "response.data");
-        if (response.data != "") {
-          console.log(response.data);
-          setCompanyData(response.data.data);
-        } else {
-          setCompanyData(null);
-          console.log("errr");
-        }
-      })
-      .catch(function (error) {
-        setLoading(false);
-        console.log("FAILED!!! ", error);
-      });
     },[])
     return ( <>
         <div className="w-full admin-dashboard overflow-x-scroll">
       <div className="flex flex-row w-full w-full items-center p-3 justify-between">
       <section class="text-black w-5/6 p-6 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800 mt-20 overflow-x-scroll">
-      <ListAllShipments iseditable={iseditable}/>
+      {todayShipments && <ListAllShipments todayShipments={todayShipments} iseditable={false}/>}
       </section>
       </div>
       </div>
