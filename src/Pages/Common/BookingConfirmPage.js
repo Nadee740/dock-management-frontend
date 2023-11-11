172b6255a58e971f69a05dc6ef7c44d2b9aeff24
Nadeem740 <nadeemblayparambil@gmail.com>
2023-11-11 15:51:35 +0530

    pdf-download-test-1

--- src/Pages/Common/BookingConfirmPage.js
+++ src/Pages/Common/BookingConfirmPage.js
@@ -13,19 +13,42 @@ const BookingConfirmPage=()=>{
     const [building,setBulding]=useState();
     const [vehicleDetails,setVehicleDetails]=useState();
     const [dockTypeDetails,setDockTypeDetails]=useState();
+    const [response,setResponse]=useState(null)
+    const handleDownloadPdf = async () => {
+        try {
+            setLoading(true)
+      const res = await fetch(`${baseUrl}/test/generate-pdf`);
+     const blob = await res.blob();
+          setResponse(blob)
     
+          // Create a link element and trigger the download
+        //   const link = document.createElement('a');
+        //   link.href = URL.createObjectURL(blob);
+        //   link.download = 'output.pdf';
+        //   document.body.appendChild(link);
+        //   link.click();
+        //   document.body.removeChild(link);
+        } catch (error) {
+          console.error('Error downloading PDF:', error);
+        }
+      };
     useEffect(()=>{
         setLoading(true);
+
         const val=sessionStorage.getItem("bookingdata");
         const v=JSON.parse(val);
-        console.log(v);
         setBookingDetails(v.data);
         setCompanyDetails(v.company);
         setBulding(v.building);
         setVehicleDetails(v.vehicle);
         setDockDetails(v.dock);
         setDockTypeDetails(v.docktype);
-        setLoading(false);
+        handleDownloadPdf().then(()=>{
+            setLoading(false);
+        }).catch((err)=>{
+            console.log(err)
+        });
+        
     },[])
 
     return (
@@ -33,7 +56,7 @@ const BookingConfirmPage=()=>{
           <div className="w-full admin-dashboard  overflow-y-scroll">
             <div className="flex flex-row w-full w-full items-center p-3 justify-between">
               <section class=" text-black ml-5 w-full p-6 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800 mt-20 overflow-scroll">
-               {bookingDetails.length && <ConfirmBooking bookingDetails={bookingDetails} buildingDetails={building} vehicleDetails={vehicleDetails} companyDetails={companyDetails} dockDetails={dockDetails} dockTypeDetails={dockTypeDetails}/> } 
+               {bookingDetails.length && response&& <ConfirmBooking response={response} bookingDetails={bookingDetails} buildingDetails={building} vehicleDetails={vehicleDetails} companyDetails={companyDetails} dockDetails={dockDetails} dockTypeDetails={dockTypeDetails}/> } 
               </section>
             </div>
           </div>
