8d0c583eda18bfe7dc7fe35dbb4012d17ef02c98
Athira-Bijoy13 <tve20cs037@cet.ac.in>
2023-12-19 18:24:20 +0530

    ui  forms1

--- src/Pages/SuperAdmin/ChangeSubscriptionStatusPage.js
+++ src/Pages/SuperAdmin/ChangeSubscriptionStatusPage.js
@@ -229,7 +229,7 @@ const ChangeSubscriptionRequestStatusPage = () => {
                 setRole(e.target.value)
             }}
               id="Vechicletype"
-              class="block w-3/5 md:2/5 lg:2/5 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             >
               <option value={-1}>---Choose Role ---</option>
               <option value={"Administrator"}>Administrator</option>
