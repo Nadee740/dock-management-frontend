c810add2f44ba729fa8f791154166f54416acc4a
Nadeem740 <nadeemblayparambil@gmail.com>
2023-12-22 00:48:54 +0530

    changed-url

--- src/Components/SideNavBar.js
+++ src/Components/SideNavBar.js
@@ -95,13 +95,7 @@ const SideNavBar = ({UserLinks}) => {
           <div class="text-gray-100 text-xl">
             <div class="p-2.5 mt-1 flex items-center">
             <a  class="flex items-center">
-            <img
-        className="fill-current h-8 w-8 mr-2"
-        src={EZlogo}  // Replace with the actual path to your local image
-        alt="Your Image Alt Text"
-        width="54"
-        height="54"
-      />
+            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
       <span class="font-semibold text-xl tracking-tight">VEZ DMS</span>
     </a>
               <i
